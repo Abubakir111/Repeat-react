85cdebcd3f125f3ea7c8e9c8ea2d2b1c46dc5b73
Бакир Базаркул <bakirbazarkul16@gmail.com>
2023-10-28 15:01:02 +0300

    error 404 button

--- src/pages/noRouting/NoRouting.jsx
+++ src/pages/noRouting/NoRouting.jsx
@@ -1,9 +1,14 @@
 import style from './noRouting.module.css';
+import { Link } from 'react-router-dom';
+import Links from '../../links/Links';
 
 const NoRouting = () => {
   return (
     <div className={style.container}>
       <h1 className={style.h1}> Такой страницы нету - ошибка 404</h1>
+      <Link to={Links.users}>
+        <button type="button">Вернуться на главную страницу</button>
+      </Link>
     </div>
   );
 };
